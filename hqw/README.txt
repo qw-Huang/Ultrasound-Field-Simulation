9bc305319935fcb24fe1f413f8a60dd43bb2f421
qw-Huang <56985137+qw-Huang@users.noreply.github.com>
2019-11-12 17:02:17 +0800

    empty

--- hqw/README.txt
+++
@@ -1,2 +0,0 @@
-This code is for rayleigh intergal, which can be used for computed sound field
-The first version is commited on 2019/11/12
--- hqw/dS2/test.m
+++
@@ -1,9 +0,0 @@
-clear all;
-dtheta=[2 3 4];
-
-ntheta =[3 4 5];
-    N=1;
-for i=1:length(ntheta)
-    theta(N:N+ntheta(i)-1)=dtheta(i):dtheta(i):dtheta(i)*ntheta(i);
-    N=N+ntheta(i);
-end
