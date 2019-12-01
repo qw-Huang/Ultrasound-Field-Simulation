dbc209dca6180a6116e71f1b1544e3f711715e28
qw-Huang <56985137+qw-Huang@users.noreply.github.com>
2019-12-01 10:21:03 +0800

    Update hole_residue_VS_hole.m

--- hole/hole_residue_VS_hole.m
+++ hole/hole_residue_VS_hole.m
@@ -1,8 +1,8 @@
-%使用dS1点声源划分方法，在2d平面xz面，加开孔的换能器声强计算
+%使用dS1点声源划分方法，在2d平面xz面，加开圆孔的换能器声强计算，比较开孔部分、剩余部分和完整换能器的声压幅值、相位
 clear all;
+close all;
 f0=1e6;%定义频率和声功率
 P=100;
-n=1*1.5;
 medium = set_medium('lossless');%定义介质（单层：水），可以改成多层set_layered_medium
 lambda = medium.soundspeed/f0;%波长=c/f
 k=2*pi/lambda;%波数
@@ -10,6 +10,7 @@ R = 1 * 5 * 2 * lambda;%ROC曲率半径
 a = 1 * 5 * lambda;%注意这里的a是孔径的一半
 fnumber=R/(2*a);%所以f-number=曲率半径/孔径（2*a）
 d = sqrt(R^2 - a^2);%理论焦点到孔径中心的距离
+u=normal_velocity(P,R,a,0,medium.density,medium.soundspeed);
 
 %划分网格点
 xmin=-a;%观察点坐标的范围
@@ -26,157 +27,99 @@ dz = lambda/6;
 x=xmin:dx:xmax;%网格点的分布
 z=zmin:dz:zmax;
 
-nx=length(x);%网格点的点数
-nz=length(z);
-
 % 求y=0时xz平面
 y=0;
 
-res_zeros_xz=zeros(nx,nz); %定义全零矩阵
-hole_zeros_xz=zeros(nx,nz);
-
-%加开孔
-hole_a=n*7.5e-4;
-u_hole=normal_velocity(P,R,a,hole_a,medium.density,medium.soundspeed);
-
-%换能器离散化为点声源  注意离散化之后点声源的大小
-dr=lambda/6;%球面分割成很多个圆环，对应的半径是r，dr是半径增加的步长
-ndr=round((a-dr-hole_a)/dr);
-dr=(a-dr-hole_a)/ndr;
-r_back=(0+hole_a):dr:a-dr;%点声源前一段弧长对应的r
-r_after=(dr+hole_a):dr:a;%点声源后一段弧长对应的r
-r=r_after-dr/2;%第i个环带对应的中心点的r
-Sm=lambda/6;%最外层环带（2pi*a）离散化后对应的dS的弧长小于lambda/6
-median=fix(length(r)/2)+1;%fix截位取整
-ntheta=round(2*pi*r(median)/Sm);%最外层一个环带的划分点数,取整
-dtheta=2*pi./ntheta;%根据取整重新调整每个点声源的对应弧度
-theta_after=dtheta:dtheta:2*pi;%每个环带离散成多个点对应的弧度数组
-theta_back=0:dtheta:(2*pi-dtheta);%每个环带离散成多个点对应的弧度数组
-theta=theta_after-dtheta/2;%第i个环带离散成点声源，dS中间的点对应的弧度数组
-X=cos(theta)'*r;%点源的x坐标  矩阵形式 ntheta*nr
-Y=sin(theta)'*r;%点源的y坐标
-Z0=R-sqrt(R*R-r.*r);%点声源三维空间中的z坐标
-Z=repmat(Z0,ntheta,1); % repmat( A , m , n )：将向量／矩阵在垂直方向复制m次，在水平方向复制n次。
-
-%rayleigh积分计算xz平面的声场  
-tic
-for ix=1:nx  %观察网格点x方向的坐标
-    for iz=1:nz  %观察网格点z方向的坐标
-        rn=sqrt((X-x(ix)).^2+(Y-y).^2+(Z-z(iz)).^2);%观察点到点源的距离
-        dS_ring=r.*dtheta.*R.*(asin(r_after./R)-asin(r_back./R));%每一环带离散的dS的大小，第i环离散的点声源面积dS=第i环的离散弧长*dr对应的短弧
-        dS=repmat(dS_ring,ntheta,1); % repmat( A , m , n )：将向量／矩阵在垂直方向复制m次，在水平方向复制n次。
-        A=dS.*exp(-1i.*k.*rn)./rn;
-        B=sum(sum(A));%对上述求得的值累加
-        pr_hole(ix,iz)=1i*medium.density*u_hole*medium.soundspeed*k/(2*pi)*B; %乘以相关参数得到声压p
-    end
-end
-toc
-%声压转化为声强计算
-I_pr_hole=acousticintensity(pr_hole,medium.density,medium.soundspeed); 
-
-%不加开孔
-hole_a=0;
-u=normal_velocity(P,R,a,hole_a,medium.density,medium.soundspeed);
-
-%换能器离散化为点声源  注意离散化之后点声源的大小
-dr=lambda/6;%球面分割成很多个圆环，对应的半径是r，dr是半径增加的步长
-ndr=round((a-dr-hole_a)/dr);
-dr=(a-dr-hole_a)/ndr;
-r_back=(0+hole_a):dr:a-dr;%点声源前一段弧长对应的r
-r_after=(dr+hole_a):dr:a;%点声源后一段弧长对应的r
-r=r_after-dr/2;%第i个环带对应的中心点的r
-Sm=lambda/6;%最外层环带（2pi*a）离散化后对应的dS的弧长小于lambda/6
-median=fix(length(r)/2)+1;%fix截位取整
-ntheta=round(2*pi*r(median)/Sm);%最外层一个环带的划分点数,取整
-dtheta=2*pi./ntheta;%根据取整重新调整每个点声源的对应弧度
-theta_after=dtheta:dtheta:2*pi;%每个环带离散成多个点对应的弧度数组
-theta_back=0:dtheta:(2*pi-dtheta);%每个环带离散成多个点对应的弧度数组
-theta=theta_after-dtheta/2;%第i个环带离散成点声源，dS中间的点对应的弧度数组
-X=cos(theta)'*r;%点源的x坐标  矩阵形式 ntheta*nr
-Y=sin(theta)'*r;%点源的y坐标
-Z0=R-sqrt(R*R-r.*r);%点声源三维空间中的z坐标
-Z=repmat(Z0,ntheta,1); % repmat( A , m , n )：将向量／矩阵在垂直方向复制m次，在水平方向复制n次。
-
-%rayleigh积分计算xz平面的声场  
-tic
-for ix=1:nx  %观察网格点x方向的坐标
-    for iz=1:nz  %观察网格点z方向的坐标
-        rn=sqrt((X-x(ix)).^2+(Y-y).^2+(Z-z(iz)).^2);%观察点到点源的距离
-        dS_ring=r.*dtheta.*R.*(asin(r_after./R)-asin(r_back./R));%每一环带离散的dS的大小，第i环离散的点声源面积dS=第i环的离散弧长*dr对应的短弧
-        dS=repmat(dS_ring,ntheta,1); % repmat( A , m , n )：将向量／矩阵在垂直方向复制m次，在水平方向复制n次。
-        A=dS.*exp(-1i.*k.*rn)./rn;
-        B=sum(sum(A));%对上述求得的值累加
-        pr(ix,iz)=1i*medium.density*u*medium.soundspeed*k/(2*pi)*B; %乘以相关参数得到声压p
-    end
-end
-toc
-%声压转化为声强计算
-I_pr=acousticintensity(pr,medium.density,medium.soundspeed); 
+%加开孔剩余部分
+hole_a=15e-4;
+pr_res=hole_rayleigh2(R,a,u,hole_a,x,z); %乘以相关参数得到声压p
 
 %被挖掉的开孔
-hole_a=0;
-a=n*7.5e-4;
-u=normal_velocity(P,R,a,hole_a,medium.density,medium.soundspeed);
+pr_hole=hole_rayleigh2(R,hole_a,u,0,x,z); %乘以相关参数得到声压p
 
-%换能器离散化为点声源  注意离散化之后点声源的大小
-dr=lambda/6;%球面分割成很多个圆环，对应的半径是r，dr是半径增加的步长
-ndr=round((a-dr-hole_a)/dr);
-dr=(a-dr-hole_a)/ndr;
-r_back=(0+hole_a):dr:a-dr;%点声源前一段弧长对应的r
-r_after=(dr+hole_a):dr:a;%点声源后一段弧长对应的r
-r=r_after-dr/2;%第i个环带对应的中心点的r
-Sm=lambda/6;%最外层环带（2pi*a）离散化后对应的dS的弧长小于lambda/6
-median=fix(length(r)/2)+1;%fix截位取整
-ntheta=round(2*pi*r(median)/Sm);%最外层一个环带的划分点数,取整
-dtheta=2*pi./ntheta;%根据取整重新调整每个点声源的对应弧度
-theta_after=dtheta:dtheta:2*pi;%每个环带离散成多个点对应的弧度数组
-theta_back=0:dtheta:(2*pi-dtheta);%每个环带离散成多个点对应的弧度数组
-theta=theta_after-dtheta/2;%第i个环带离散成点声源，dS中间的点对应的弧度数组
-X=cos(theta)'*r;%点源的x坐标  矩阵形式 ntheta*nr
-Y=sin(theta)'*r;%点源的y坐标
-Z0=R-sqrt(R*R-r.*r);%点声源三维空间中的z坐标
-Z=repmat(Z0,ntheta,1); % repmat( A , m , n )：将向量／矩阵在垂直方向复制m次，在水平方向复制n次。
+%完整换能器
+pr_all=hole_rayleigh2(R,a,u,0,x,z); %乘以相关参数得到声压p
 
-%rayleigh积分计算xz平面的声场  
-tic
-for ix=1:nx  %观察网格点x方向的坐标
-    for iz=1:nz  %观察网格点z方向的坐标
-        rn=sqrt((X-x(ix)).^2+(Y-y).^2+(Z-z(iz)).^2);%观察点到点源的距离
-        dS_ring=r.*dtheta.*R.*(asin(r_after./R)-asin(r_back./R));%每一环带离散的dS的大小，第i环离散的点声源面积dS=第i环的离散弧长*dr对应的短弧
-        dS=repmat(dS_ring,ntheta,1); % repmat( A , m , n )：将向量／矩阵在垂直方向复制m次，在水平方向复制n次。
-        A=dS.*exp(-1i.*k.*rn)./rn;
-        B=sum(sum(A));%对上述求得的值累加
-        pr_h(ix,iz)=1i*medium.density*u*medium.soundspeed*k/(2*pi)*B; %乘以相关参数得到声压p
-    end
-end
-toc
-%声压转化为声强计算
-I_pr_h=acousticintensity(pr_h,medium.density,medium.soundspeed); 
+% %选择感兴趣区域误差
+% [index]=find(I_pr>=0.25*I_pr_max);
+% contribution_res=I_pr_res./I_pr;
+% contribution_hole=I_pr_hole./I_pr;
+% res_zeros_xz(index)=contribution_res(index);
+% hole_zeros_xz(index)=contribution_hole(index);
 
-I_pr_max=max(I_pr(:));%无开孔声强最大值
-%选择感兴趣区域误差
-[index]=find(I_pr>=0.25*I_pr_max);
-contribution_res=I_pr_hole./I_pr;
-contribution_hole=I_pr_h./I_pr;
-res_zeros_xz(index)=contribution_res(index);
-hole_zeros_xz(index)=contribution_hole(index);
-
+%比较声压的幅值和相位
+pr_all_amplitude=abs(pr_all);
+pr_all_phase=angle(pr_all);
+pr_sum=pr_hole+pr_res;
+pr_sum_amplitude=abs(pr_sum);
+pr_sum_phase=angle(pr_sum);
+error_phase=abs(pr_sum_phase-pr_all_phase)./pr_all_phase;
+error_amplitude=abs(pr_sum_amplitude-pr_all_amplitude)./pr_all_amplitude;
+I_all=abs(pr_all).^2./(2*medium.soundspeed*medium.density);
+I_hole=abs(pr_hole).^2./(2*medium.soundspeed*medium.density);
+I_res=abs(pr_res).^2./(2*medium.soundspeed*medium.density);
+max_hole=find_maxpoint(I_hole);
+max_res=find_maxpoint(I_res);
+max_all=find_maxpoint(I_all);
 % %画图
-% 对比剩余部分和去掉部分对声场分布的贡献
+% % 对比剩余部分和去掉部分对声场分布的贡献
+figure(1);
+surf(z*1000, x*1000,pr_all_amplitude); 
+axis equal;
+shading interp;
+colorbar;
+xlabel('z (mm) ');
+ylabel('x (mm) ');
+title('pr_all_amplitude(R=15mm,a=7.5mm,20%a)');
 figure(2);
-surf(z*1000, x*1000, hole_zeros_xz); 
-shading interp %去掉网格，平滑曲面
+surf(z*1000, x*1000,abs(pr_hole));
 axis equal;
-colorbar  %加颜色条
+shading interp;
+colorbar;
 xlabel('z (mm) ');
 ylabel('x (mm) ');
-title('(R=15mm,a=7.5mm,10%a)');
+title('pr_hole_amplitude(R=15mm,a=7.5mm,20%a)');
 figure(3);
-surf(z*1000, x*1000, res_zeros_xz); 
-shading interp %去掉网格，平滑曲面
+surf(z*1000, x*1000,abs(pr_res));
 axis equal;
-colorbar  %加颜色条
+shading interp;
+colorbar;xlabel('z (mm) ');
+ylabel('x (mm) ');
+title('pr_res_amplitude(R=15mm,a=7.5mm,20%a)');
+ figure(4);
+surf(z*1000, x*1000,error_amplitude); 
+axis equal;
+shading interp;
+colorbar;
 xlabel('z (mm) ');
 ylabel('x (mm) ');
-title('(R=15mm,a=7.5mm,10%a)');
+title('the error of amplitude(R=15mm,a=7.5mm,20%a)');
+figure(5);
+surf(z*1000, x*1000,error_phase);
+axis equal;
+shading interp;
+colorbar;
+xlabel('z (mm) ');
+ylabel('x (mm) ');
+title('the error of phase(R=15mm,a=7.5mm,20%a)');
 
+% figure(6);
+% plot(abs(pr_all(31,:)));
+% hold on;
+% plot(abs(pr_res(31,:)));
+% hold on;
+% plot(abs(pr_hole(31,:)));
+% figure(7);
+% plot(angle(pr_all(31,:)));
+% hold on;
+% plot(angle(pr_res(31,:)));
+% hold on;
+% plot(angle(pr_hole(31,:)));
+
+% shading interp ;%去掉网格，平滑曲面
+% axis equal;
+% colorbar  %加颜色条
+% xlabel('z (mm) ');
+% ylabel('x (mm) ');
+% title('(R=15mm,a=7.5mm,30%a)');
+% save('R15_hole0.3a_holeVSresidue.mat','R','a','pr','pr_hole','pr_res','I_pr_hole','I_pr_res','I_pr','res_zeros_xz','hole_zeros_xz');
