07e183fe11218bb889ab5f112ba0ce5cb09e5706
qw-Huang <56985137+qw-Huang@users.noreply.github.com>
2019-12-05 21:07:08 +0800

    Update diffp.m

--- equation numerical solution/diffp.m
+++ equation numerical solution/diffp.m
@@ -9,12 +9,14 @@ u=1;
 A=15e-3;
 k=2*pi/lambda;%²¨Êý
 y2=pi/5;
-y1=0.35*y2;
+% y1=0.35*y2;
+y1=0;
+n=sin(y1)/sin(y2);
 U2=sqrt(w.^2+2*A.*(A-w).*(1-cos(y2)));
 U1=sqrt(w.^2+2*A.*(A-w).*(1-cos(y1)));
 p=2*medium.density*medium.soundspeed*u*A./abs(A-w).*abs(sin(k.*(U2-U1)./2));
 dp=diff(p,w);
 w0=vpasolve(dp==0,w,14e-3);
-w0
+w1=subs(w0);
 % w=0:2.5e-4:14e-3;
 
