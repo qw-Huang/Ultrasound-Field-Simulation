ca29645f4bea985935f08dc161a8870eb414fd61
qw-Huang <56985137+qw-Huang@users.noreply.github.com>
2019-12-25 09:26:31 +0800

    Update FNM_ASA.m

--- FNM/FNM_ASA.m
+++ FNM/FNM_ASA.m
@@ -1,21 +1,21 @@
 %内置FNM(FOCUS)计算一个平面，然后用ASA计算三维空间声场分布
 clc;clear all;
 f0=1e6;u=1;%定义频率和法向阵速
-a = 5 * 1.5e-3;%注意这里a是孔径的一半
-R = 2*a;%ROC曲率半径
-medium = set_medium('lossless');%定义介质：单层->水
+a = 30e-3;%注意这里a是孔径的一半
+R = 75e-3;%ROC曲率半径
+medium = set_medium('water');%定义介质：单层->水
 lambda = medium.soundspeed/f0;%波长=c/f 1.5mm   lambda/6=0.25mm
 k=2*pi/lambda;%波数
 fnumber=1;
 d = sqrt(R^2 - a^2);%理论焦点到孔径中心的距离
 %划分网格点
-xmin=-0.7*a;%观察点坐标的范围
+xmin=-1.5*a;%观察点坐标的范围
 xmax=-xmin;
-ymax=0.7*a;
+ymax=1.5*a;
 ymin=-ymax;
-zDiff=0.7*d;
-zmin=R-zDiff;
-zmax=R+zDiff;
+
+zmin=30e-3;
+zmax=30e-3;
 
 dx = lambda/6; %网格点的步长
 dy = lambda/6; 
@@ -50,21 +50,9 @@ fprintf('done in %f s.\n', toc());
 
 fprintf('Calculating 3D pressure (%i points) with ASA... ', (length(x) * length(y) * length(z)));
 tic();
-p_asa = cw_angular_spectrum(p0,cg_3d,medium,f0,1024,'Pa');
+% p_asa = cw_angular_spectrum(p0,cg_3d,medium,f0,1024,'Pa');
 fprintf('done in %f s.\n', toc());
 
 % Show the initial pressure
-figure(1);
-mesh(x*1000, y*1000, rot90(abs(squeeze(p0(:,:,1)))));
-xlabel('x (mm)');
-ylabel('y (mm)');
-shading flat;
-title(['p0 (Calculated with FNM at z = ', num2str(z0*1000), ' mm)']);
-% Show the 3D field calculated with ASA
-figure(2);
-p_normalized=abs(squeeze(p_asa(:,y_index,:)))/max(max(abs(squeeze(p_asa(:,y_index,:)))));
-mesh(z*1000, x*1000, p_normalized);
-xlabel('z (mm)');
-ylabel('x (mm)');
-shading flat;
-title('normalized ASA Pressure (y=0)');
+pr=rayleigh_2D_xy(R,a,f0,u,medium,x,y,zmax);
+error=abs(pr-p_asa(:,:,length(z)))./abs(pr);
